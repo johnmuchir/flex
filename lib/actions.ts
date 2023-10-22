39dc6065cd1b2a53f31ff7c2bfbe8eb3deb6035d
johnmuchir <johnmuchiri1099@gmail.com>
2023-10-22 09:50:44 +0300

    first commit

---
+++ lib/actions.ts
@@ -0,0 +1,17 @@
+import { GraphQLClient } from "graphql-request";
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const client = new GraphQLClient('apiUrl')
+
+const makeGraphQLRequest = async (query: string, variables = {}) => {
+    try {
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const getUser = (email: string) => {
+    
+}
